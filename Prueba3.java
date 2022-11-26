7da616060fd0ae76b4ae1df804ad698d1a4f4103
Juan Marcos <juanmarcosleon1990@gmail.com>
2022-11-26 23:33:37 +0000

    Práctica 1 Ejercicio 3 arreglado

--- Prueba3.java
+++ Prueba3.java
@@ -1,19 +1,24 @@
+import java.util.Scanner;
+
 public class Prueba3 {
+
     public static void mostrartablasparcialmente(int a, int b) {
         int valor;
         for (int i = 1; i <= a; i++) {
-            for (int j = 1; j <= a; j++) {
+            for (int j = 1; j <= b; j++) {
                 valor = i * j;
-                valor = valor + 1;
-                System.out.println(i + " * " + j + " = " + (valor + b));
+                System.out.println(i + " * " + j + " = " + valor);
             }
             System.out.println("--------");
         }
     }
 
     public static void main(String[] args) {
-        int x = 5;
-        int y = 2;
+        Scanner dato = new Scanner(System.in);
+        System.out.print("Indique hasta que número desea ver las tablas: ");
+        int x = dato.nextInt();
+        System.out.print("Indique el número hasta el que desea multiplicar: ");
+        int y = dato.nextInt();
         mostrartablasparcialmente(x, y);
     }
 }
